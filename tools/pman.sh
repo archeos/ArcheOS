bd48212647a79294673b384cf1be22974bf37a98
Fabrizio Furnari <fab.furnari@gmail.com>
2010-08-17 15:01:36 +0200

    Improved pman

--- tools/pman.sh
+++ tools/pman.sh
@@ -6,12 +6,15 @@ if [ $1 == "-x" ]; then
     mkdir -p $PACKAGE_NAME/DEBIAN
     dpkg --control $2 $PACKAGE_NAME/DEBIAN
     dpkg -x $2 $PACKAGE_NAME
-fi
-if [ $1 == "-b" ]; then
+elif [ $1 == "-b" ]; then
     sudo chown -R root:root $2
     dpkg -b $2 $2.deb
+    echo
+    echo
     lintian $2.deb
+else
+	echo "Usage: $0 -b|-x TARGET"
+	echo 
 fi
 
 
-echo "finito!"
