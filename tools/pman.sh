07efeaa927375599340651ec37194bdebcf9eed9
Fabrizio Furnari <fab.furnari@gmail.com>
2010-11-15 11:14:59 +0100

    Some modifications to pman.sh script      * added md5sum feature      * fixed trailing slash bug

--- tools/pman.sh
+++ tools/pman.sh
@@ -8,10 +8,14 @@ if [ $1 == "-x" ]; then
     dpkg -x $2 $PACKAGE_NAME
 elif [ $1 == "-b" ]; then
     sudo chown -R root:root $2
-    dpkg -b $2 $2.deb
+    pack_name=$(echo "$2" | sed -e "s/\/*$//")
+    cd $pack_name
+    md5sum $(find . -type f | grep -v '^\.\/DEBIAN/') > DEBIAN/md5sums
+    cd ..
+    dpkg -b $pack_name $pack_name.deb
     echo
     echo
-    lintian $2.deb
+    lintian $pack_name.deb
 else
 	echo "Usage: $0 -b|-x TARGET"
 	echo 
