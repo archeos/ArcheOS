abeaf70b810c33b14f168bad3655e425b0663981
Fabrizio Furnari <fab.furnari@gmail.com>
2010-11-15 11:29:40 +0100

    Added some echoes...

--- tools/pman.sh
+++ tools/pman.sh
@@ -7,11 +7,15 @@ if [ $1 == "-x" ]; then
     dpkg --control $2 $PACKAGE_NAME/DEBIAN
     dpkg -x $2 $PACKAGE_NAME
 elif [ $1 == "-b" ]; then
+    echo "   * Setting permissions..."
     sudo chown -R root:root $2
+    echo "   * Removing trailing slash (if present)..."
     pack_name=$(echo "$2" | sed -e "s/\/*$//")
     cd $pack_name
+    echo "   * Generating the md5sum file..."
     md5sum $(find . -type f | grep -v '^\.\/DEBIAN/') > DEBIAN/md5sums
     cd ..
+    echo "   * And finally let's buld the package!'"
     dpkg -b $pack_name $pack_name.deb
     echo
     echo
