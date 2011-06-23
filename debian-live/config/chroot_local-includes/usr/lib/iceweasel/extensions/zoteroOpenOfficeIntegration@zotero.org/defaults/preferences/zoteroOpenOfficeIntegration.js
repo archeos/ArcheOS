97be8818f02accf984b2d7d5f575203ea77f497f
Fabrizio Furnari <fab.furnari@gmail.com>
2011-06-23 10:36:01 +0200

    Removed manually entered zotero extension. Hooray!

--- debian-live/config/chroot_local-includes/usr/lib/iceweasel/extensions/zoteroOpenOfficeIntegration@zotero.org/defaults/preferences/zoteroOpenOfficeIntegration.js
+++
@@ -1,3 +0,0 @@
-pref("extensions.zoteroOpenOfficeIntegration.urePath", "");
-pref("extensions.zoteroOpenOfficeIntegration.sofficePath", "");
-pref("extensions.zoteroOpenOfficeIntegration.version", "");
